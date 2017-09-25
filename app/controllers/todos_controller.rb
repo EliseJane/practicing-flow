6dddf11390af2fe245607d553bf7ff8ba0843589
Elise Olivares <elisejpo@gmail.com>
2017-09-25 12:44:44 -0700

    el2

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -23,15 +23,30 @@ class TodosController < ApplicationController
 
   def update
     @todo = Todo.find(params[:id])
-    binding.pry
-    if @todo.update(completed: params[:completed])
-      flash[:notice] = 'This Todo was marked completed.'
-      redirect_to todos_path
+    if params[:completed]
+      if @todo.update(completed: params[:completed])
+        flash[:notice] = 'This Todo was marked completed.'
+        redirect_to todos_path
+      else
+        render :show
+      end
     else
-      render :show
+      @todo.duedate = Time.new(params[:year], params[:month], params[:day])
+      if @todo.update(todo_params)
+        flash[:notice] = 'This Todo was updated.'
+        redirect_to todos_path
+      else
+        render :show
+      end
     end
   end
 
   def destroy
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit!
   end
 end
