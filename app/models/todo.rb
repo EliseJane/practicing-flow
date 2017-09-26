49b51265ce08462dc0963d00e485ece095fede74
Sunny <sjbeatteay@gmail.com>
2017-09-26 16:35:59 -0400

    wip

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,4 @@
 class Todo < ActiveRecord::Base
+  belongs_to :user
   validates_presence_of :title, :description, :duedate
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926193736) do
+ActiveRecord::Schema.define(version: 20170926201027) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170926193736) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170926193736) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
